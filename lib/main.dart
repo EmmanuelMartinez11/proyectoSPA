67993ae9c6d22a6a69bcf71bf2f6837aa3faab33
EmmanuelMartinez11 <emma.nionn@gmail.com>
2024-08-12 21:57:48 -0300

    backend

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:proyecto_flutter/firebase_options.dart';
 import 'widgets/ingresar/ingresar.dart';
 import 'widgets/inicio/inicio.dart';
 import 'widgets/servicios/servicios.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 
@@ -16,7 +22,8 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      initialRoute: '/inicio', // Define la página de inicio como la ruta inicial
+      initialRoute:
+          '/inicio', // Define la página de inicio como la ruta inicial
       routes: {
         '/inicio': (context) => Inicio(),
         '/servicios': (context) => Servicios(),
