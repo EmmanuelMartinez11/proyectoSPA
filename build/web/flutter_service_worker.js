531f69e223ac705010eee6cc2258e518a761c5e3
EmmanuelMartinez11 <emma.nionn@gmail.com>
2024-10-22 18:38:58 -0300

    final

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -69,7 +69,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "d848fa3235ec444e0fba8d5c82991f34
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "9fc2555b74061f2f6f67cce5b43325dd",
+"flutter_bootstrap.js": "7c049d9e2ef5405e3dc0b7244ea606f0",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
